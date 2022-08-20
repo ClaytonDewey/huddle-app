8c64be45656dd08801c6c0dec35f968218b22aaa
ClaytonDewey <clay@dryadmedia.com>
2022-08-20 10:02:40 -0500

    Import Card & Footer components
    Map through content, and output

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import { ThemeProvider } from "styled-components";
 import Header from "./components/Header";
+import Card from "./components/Card";
+import Footer from "./components/Footer";
 import { Container } from "./components/styles/Container.styled";
 import GlobalStyles from "./components/styles/Global";
+import content from "./content";
 // https://www.youtube.com/watch?v=02zO0hZmwnw 27:26
 
 const theme = {
@@ -10,6 +13,7 @@ const theme = {
     body: "#fff",
     footer: "#003333",
   },
+  mobile: "768px",
 };
 
 function App() {
@@ -19,8 +23,11 @@ function App() {
         <GlobalStyles />
         <Header />
         <Container>
-          <h1>Hello World</h1>
+          {content.map((item, index) => {
+            return <Card key={index} item={item} />;
+          })}
         </Container>
+        <Footer />
       </>
     </ThemeProvider>
   );
